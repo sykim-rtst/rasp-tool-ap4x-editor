8a542e11f802f0d81edba67f689d308cb08f79db
thkim <thkim>
2018-10-12 19:23:44 +0000

    - ¸¶¹ý»ç ¼öÁ¤ ÁøÇàÁß

--- src/kr/co/rtst/autosar/ap4x/editor/AdaptiveAUTOSARFormEditor.java
+++ src/kr/co/rtst/autosar/ap4x/editor/AdaptiveAUTOSARFormEditor.java
@@ -1,7 +1,33 @@
 package kr.co.rtst.autosar.ap4x.editor;
 
 import org.artop.aal.examples.editor.AutosarFormEditor;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
 
 public abstract class AdaptiveAUTOSARFormEditor extends AutosarFormEditor/*BasicTransactionalFormEditor*/ {
+	
+	@Override
+	public void init(IEditorSite site, IEditorInput editorInput) {
+		// TODO Auto-generated method stub
+		System.out.println("Input:"+editorInput);
+		super.init(site, editorInput);
+		
+		Object o = getEditorInputObject();
+		System.out.println("::::::::"+o);
+		System.out.println("---"+getEditorInputName());
+	}
+	
+	@Override
+	public void doSave(IProgressMonitor monitor) {
+		// TODO Auto-generated method stub
+		super.doSave(monitor);
+	}
+	
+	@Override
+	public void editorDirtyStateChanged() {
+		// TODO Auto-generated method stub
+		super.editorDirtyStateChanged();
+	}
 
 }
